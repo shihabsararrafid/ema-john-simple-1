bed489a2baf7bcc098caac9974542b1bb475b7e1
shiabsararrafid <shrafid.532@gmail.com>
2022-03-26 09:12:11 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>I am Rafid . And it is the header.</h1>
+        </div>
+    );
+};
+
+export default Header;
