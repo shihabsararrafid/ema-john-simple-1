b0e2dfe03e6efd1390be515bc94da9a60ef165aa
shiabsararrafid <shrafid.532@gmail.com>
2022-03-26 11:51:49 +0600

    product-info style applied

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const { name, img, category, seller, price } = props.product;
+    const { name, img, category, seller, price, ratings } = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p className='product-price'>Price:${price}</p>
+                <p><small>Manufacturer:{seller}</small></p>
+                <p><small>Ratings:{ratings}</small></p>
+            </div>
+            <button className='addCartBtn'>Add To Cart</button>
         </div>
     );
 };
